c1603186d30836a4dfecfad97dd0ca7dca251511
eb247@hotmail.com <eb247@hotmail.com>
2018-08-08 14:17:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  var lowercase = string.toLowerCase()
-  var uppercase = string.toUpperCase()
+  var lowercase = "hello"
+  var uppercase = "HELLO"
   if(string.toLowerCase() === lowercase) {
     return "I can\'t hear you!"
   } else if (string.toUpperCase() === uppercase) {
